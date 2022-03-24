acd899586db86d16b8c23a670639f8ed5ce26277
ThaynaraLimaa <thaynaralimaa61@gmail.com>
2022-03-24 13:37:43 -0300

    Adicionei o menu responsivo
    Finalmente fiz o menu hamburguer para as telas menores e também adicionei um scrollbar personalizado para o site.

---
+++ script.js
@@ -0,0 +1,5 @@
+let humburguer = document.querySelector('.hamburguer')
+
+humburguer.addEventListener('click', function(){
+    document.querySelector('.container-menu').classList.toggle('show-menu')
+})
